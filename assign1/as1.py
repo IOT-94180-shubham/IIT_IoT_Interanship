f97afe1ee8b52552dff705514a69bb1bd7149dac
Shubhk13 <shubhamkore121@gmail.com>
2025-12-12 13:00:12 +0530

    assign1

---
+++ assign1/as1.py
@@ -0,0 +1,10 @@
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+print("Addition =", addition)
+print("Subtraction =", subtraction)
+print("Multiplication =", multiplication)
+print("Division =", division)
---
+++ assign1/asss1.py
@@ -0,0 +1,8 @@
+a = input("Enter first number: ")
+b = input("Enter second number: ")
+temp = a
+a = b
+b = temp
+print("After swapping:")
+print("First number =", a)
+print("Second number =", b)
